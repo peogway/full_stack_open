0b9566b2ba7e9aadb24a09b329ccf8d3e6332547
peogway <duchungbeo2004@gmail.com>
2024-07-13 22:34:51 +0300

    update blog list with tests

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -1,23 +1,48 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response,next) => {
-  const blog = new Blog(request.body)
+blogsRouter.post('/', async (request, response) => {
+  const body = request.body
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(error => next(error))
+  if (!('title' in body) || !('url' in body)) {
+    response.status(400).end()
+    return
+  }
+
+
+  if (!('likes' in body)) body.likes =0
+
+  const blog = new Blog(body)
+
+
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 })
+
+blogsRouter.get('/:id', async (request, response) => {
+  const blog  = await Blog.findById(request.params.id)
+  response.json(blog)
+})
+
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndDelete(request.params.id)
+  response.status(204).end()
+})
+
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    likes: body.likes
+  }
+
+  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
+  response.status(204).end()
+})
+
 
 module.exports = blogsRouter
--- part4/blogList/utils/config.js
+++ part4/blogList/utils/config.js
@@ -1,6 +1,11 @@
 require('dotenv').config()
 
 let PORT = process.env.PORT
-let MONGODB_URI = process.env.MONGODB_URI
+const MONGODB_URI = process.env.NODE_ENV === 'test'
+  ? process.env.TEST_MONGODB_URI
+  : process.env.MONGODB_URI
 
-module.exports = { PORT, MONGODB_URI }
+module.exports = {
+  MONGODB_URI,
+  PORT
+}
