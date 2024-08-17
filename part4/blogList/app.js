6c8dd2c00a9538cac2db3980832618bec3db686b
peogway <duchungbeo2004@gmail.com>
2024-08-17 05:30:31 +0300

    update app.js

--- part4/blogList/app.js
+++ part4/blogList/app.js
@@ -25,7 +25,6 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors());
 app.use(express.static("dist"));
 app.use(express.json());
-app.use(middleware.requestLogger);
 
 app.use(middleware.tokenExtractor);
 
