d7d22147c49fd781564c0a39d62a7073855f6e27
peogway <duchungbeo2004@gmail.com>
2024-08-19 22:07:38 +0300

    update be post request

--- part4/blogList/controllers/blogs.js
+++ part4/blogList/controllers/blogs.js
@@ -37,7 +37,13 @@ blogsRouter.post("/", async (request, response) => {
   user.blogs = user.blogs.concat(savedBlog.id);
   await user.save();
 
-  response.status(201).json(savedBlog);
+  const populatedBlog = await Blog.findById(savedBlog.id).populate("user", {
+    id: 1,
+    name: 1,
+    username: 1,
+  });
+
+  response.status(201).json(populatedBlog);
 });
 
 blogsRouter.get("/:id", async (request, response) => {
