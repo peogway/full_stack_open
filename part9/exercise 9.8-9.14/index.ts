ff852da92c2e2480ebfdc06e946d0ce57fb21e41
peogway <duchungbeo2004@gmail.com>
2025-08-20 14:00:23 +0300

    Add request validation and error handling for new patient endpoint

--- part9/exercise 9.8-9.14/index.ts
+++ part9/exercise 9.8-9.14/index.ts
@@ -1,10 +1,33 @@
-import express from 'express'
+import express, { NextFunction, Request, Response } from 'express'
 import cors from 'cors'
+import * as z from 'zod'
 
 import diagnoseService from './services/diagnoseService'
 import patientService from './services/patientService'
-import toNewPatient from './utils'
+import { NewPatientSchema } from './utils'
+import { Patient, NewPatient } from './types/PatientType'
 // import { Diagnose, DiagnoseWithoutLatin } from './types'
+
+const newPatientParser = (req: Request, _res: Response, next: NextFunction) => {
+	try {
+		NewPatientSchema.parse(req.body)
+		next()
+	} catch (error: unknown) {
+		next(error)
+	}
+}
+const errorMiddleware = (
+	error: unknown,
+	_req: Request,
+	res: Response,
+	next: NextFunction
+) => {
+	if (error instanceof z.ZodError) {
+		res.status(400).send({ error: error.issues })
+	} else {
+		next(error)
+	}
+}
 
 const app = express()
 app.use(cors())
@@ -19,24 +42,22 @@ app.get('/api/ping', (_req, res) => {
 app.get('/api/diagnose', (_req, res) => {
 	res.send(diagnoseService.getDiagnosesWithoutLatin())
 })
+
 app.get('/api/patients', (_req, res) => {
 	console.log('someone requested patients')
 	res.send(patientService.getPatientsWithoutSensitiveInfo())
 })
-app.post('/api/patients', (req, res) => {
-	try {
-		const newPatient = toNewPatient(req.body)
 
-		const addedEntry = patientService.addPatient(newPatient)
+app.post(
+	'/api/patients',
+	newPatientParser,
+	(req: Request<unknown, unknown, NewPatient>, res: Response<Patient>) => {
+		const addedEntry = patientService.addPatient(req.body)
 		res.json(addedEntry)
-	} catch (error: unknown) {
-		let errorMessage = 'Something went wrong.'
-		if (error instanceof Error) {
-			errorMessage += ' Error: ' + error.message
-		}
-		res.status(400).send(errorMessage)
 	}
-})
+)
+
+app.use(errorMiddleware)
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
