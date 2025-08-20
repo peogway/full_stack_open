081dff1f44652d16f203db79691077a69889ca2a
peogway <duchungbeo2004@gmail.com>
2025-08-20 12:43:53 +0300

    Add endpoints for retrieving diagnoses and patients without sensitive info

--- part9/exercise 9.8-9.14/index.ts
+++ part9/exercise 9.8-9.14/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
 import cors from 'cors'
 
+import diagnoseService from './services/diagnoseService'
+import patientService from './services/patientService'
+// import { Diagnose, DiagnoseWithoutLatin } from './types'
+
 const app = express()
 app.use(cors())
 app.use(express.json())
@@ -11,12 +15,14 @@ app.get('/api/ping', (_req, res) => {
 	res.send('pong')
 })
 
+app.get('/api/diagnose', (_req, res) => {
+	res.send(diagnoseService.getDiagnosesWithoutLatin())
+})
 app.get('/api/patients', (_req, res) => {
 	console.log('someone requested patients')
-	res.send('patients')
+	res.send(patientService.getPatientsWithoutSensitiveInfo())
 })
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`)
 })
-
