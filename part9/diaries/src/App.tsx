b7c4e6c1c4e9d7e64fc585c26ab70a54ab6480ed
peogway <duchungbeo2004@gmail.com>
2025-08-22 05:28:48 +0300

    Implement main App component to manage diary entries and form submissions

--- part9/diaries/src/App.tsx
+++ part9/diaries/src/App.tsx
@@ -1,3 +1,36 @@
-const App = () => {}
+import DiaryEntries from './components/DiaryEntries'
+import DiaryForm from './components/DiaryForm'
+import { useState, useEffect } from 'react'
+import { getDiaries } from './services/diaryService'
+
+import { Diary } from './types'
+
+const App = () => {
+	const [diaries, setDiaries] = useState<Diary[]>([])
+	const [newDiary, setNewDiary] = useState<Diary | null>(null)
+
+	useEffect(() => {
+		const fetchDiaries = async () => {
+			const allDiaries = await getDiaries()
+			setDiaries(allDiaries)
+		}
+		fetchDiaries()
+	}, [])
+
+	useEffect(() => {
+		if (newDiary) {
+			setDiaries((prevDiaries) => [...prevDiaries, newDiary])
+			setNewDiary(null) // Reset newDiary after adding it to the list
+		}
+	}, [newDiary])
+
+	return (
+		<div>
+			<DiaryForm setNewDiary={setNewDiary} />
+			<DiaryEntries diaries={diaries} />
+		</div>
+	)
+}
 
 export default App
+
