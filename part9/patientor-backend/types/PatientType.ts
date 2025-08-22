5a0fd3eb4395ad1c23381d87eb7c809157ec3abc
peogway <duchungbeo2004@gmail.com>
2025-08-23 00:39:04 +0300

    Refactor patient type schemas and remove utils file

--- part9/patientor-backend/types/PatientType.ts
+++ part9/patientor-backend/types/PatientType.ts
@@ -1,7 +1,32 @@
 import { z } from 'zod'
-import { NewPatientSchema } from '../utils'
 
-export type PatientWithoutSsn = Omit<Patient, 'ssn'>
+export const EntrySchema = z.object({
+	id: z.string(),
+	description: z.string(),
+})
+
+export enum Gender {
+	Male = 'male',
+	Female = 'female',
+	Other = 'other',
+}
+
+export type Entry = z.infer<typeof EntrySchema>
+
+export const NewPatientSchema = z.object({
+	name: z
+		.string()
+		.min(3, 'Name is required with a minimum length of 3 characters'),
+	dateOfBirth: z.string().date(),
+	ssn: z
+		.string()
+		.min(11, 'SSN is required with a minimum length of 11 characters'),
+	gender: z.nativeEnum(Gender),
+	occupation: z
+		.string()
+		.min(3, 'Occupation is required with a minimum length of 3 characters'),
+	entries: z.array(EntrySchema).default([]),
+})
 
 export type NewPatient = z.infer<typeof NewPatientSchema>
 
@@ -9,9 +34,5 @@ export interface Patient extends NewPatient {
 	id: string
 }
 
-export enum Gender {
-	Male = 'male',
-	Female = 'female',
-	Other = 'other',
-}
+export type NonSensitivePatient = Omit<Patient, 'ssn' | 'entries'>
 
