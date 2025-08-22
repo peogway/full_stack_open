a992e636c5ae06ebd086ee1770e65608f6242912
peogway <duchungbeo2004@gmail.com>
2025-08-23 01:51:09 +0300

    define type Entry

--- part9/patientor-backend/types/DianoseType.ts
+++ part9/patientor-backend/types/DianoseType.ts
@@ -1,8 +1,50 @@
-export interface Diagnose {
+export interface Diagnosis {
 	code: string
 	name: string
 	latin?: string
 }
 
-export type DiagnoseWithoutLatin = Omit<Diagnose, 'latin'>
+export type DiagnosisWithoutLatin = Omit<Diagnosis, 'latin'>
+
+export interface BaseEntry {
+	id: string
+	description: string
+	date: string
+	specialist: string
+	diagnosisCodes?: Diagnosis['code'][]
+}
+
+export enum HealthCheckRating {
+	'Healthy' = 0,
+	'LowRisk' = 1,
+	'HighRisk' = 2,
+	'CriticalRisk' = 3,
+}
+
+export interface HospitalEntry extends BaseEntry {
+	type: 'Hospital'
+	discharge: {
+		date: string
+		criteria: string
+	}
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+	type: 'OccupationalHealthcare'
+	employerName: string
+	sickLeave?: {
+		startDate: string
+		endDate: string
+	}
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+	type: 'HealthCheck'
+	healthCheckRating: HealthCheckRating
+}
+
+export type Entry =
+	| HospitalEntry
+	| OccupationalHealthcareEntry
+	| HealthCheckEntry
 
