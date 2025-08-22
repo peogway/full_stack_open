5e379fa28d27e86af8b3826b5ed7662028ad59c7
peogway <duchungbeo2004@gmail.com>
2025-08-23 01:53:58 +0300

    remove redundant interface, change to DiagnosisSchema

--- part9/patientor-backend/types/DianoseType.ts
+++ part9/patientor-backend/types/DianoseType.ts
@@ -1,50 +1,12 @@
-export interface Diagnosis {
-	code: string
-	name: string
-	latin?: string
-}
+import { z } from 'zod'
+
+export const DiagnosisSchema = z.object({
+	code: z.string(),
+	name: z.string(),
+	latin: z.string().optional(),
+})
+
+export type Diagnosis = z.infer<typeof DiagnosisSchema>
 
 export type DiagnosisWithoutLatin = Omit<Diagnosis, 'latin'>
-
-export interface BaseEntry {
-	id: string
-	description: string
-	date: string
-	specialist: string
-	diagnosisCodes?: Diagnosis['code'][]
-}
-
-export enum HealthCheckRating {
-	'Healthy' = 0,
-	'LowRisk' = 1,
-	'HighRisk' = 2,
-	'CriticalRisk' = 3,
-}
-
-export interface HospitalEntry extends BaseEntry {
-	type: 'Hospital'
-	discharge: {
-		date: string
-		criteria: string
-	}
-}
-
-export interface OccupationalHealthcareEntry extends BaseEntry {
-	type: 'OccupationalHealthcare'
-	employerName: string
-	sickLeave?: {
-		startDate: string
-		endDate: string
-	}
-}
-
-export interface HealthCheckEntry extends BaseEntry {
-	type: 'HealthCheck'
-	healthCheckRating: HealthCheckRating
-}
-
-export type Entry =
-	| HospitalEntry
-	| OccupationalHealthcareEntry
-	| HealthCheckEntry
 
