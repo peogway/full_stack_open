d090d5e82771f16791cc366c6f4271d837d74745
peogway <duchungbeo2004@gmail.com>
2025-08-23 01:50:35 +0300

    add entry data

--- part9/patientor-backend/data/patients.ts
+++ part9/patientor-backend/data/patients.ts
@@ -7,7 +7,21 @@ const data = [
 		ssn: '090786-122X',
 		gender: 'male',
 		occupation: 'New york city cop',
-		entries: [] as Entry[],
+		entries: [
+			{
+				id: 'd811e46d-70b3-4d90-b090-4535c7cf8fb1',
+				date: '2015-01-02',
+				type: 'Hospital',
+				specialist: 'MD House',
+				diagnosisCodes: ['S62.5'],
+				description:
+					"Healing time appr. 2 weeks. patient doesn't remember how he got the injury.",
+				discharge: {
+					date: '2015-01-16',
+					criteria: 'Thumb has healed.',
+				},
+			},
+		] as Entry[],
 	},
 	{
 		id: 'd2773598-f723-11e9-8f0b-362b9e155667',
