afeed188755dae8e60226811a52228884c64bfaf
peogway <duchungbeo2004@gmail.com>
2025-08-23 03:45:19 +0300

    define types for  different entries, and patients

--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -1,22 +1,66 @@
 export interface Diagnosis {
-  code: string;
-  name: string;
-  latin?: string;
+	code: string
+	name: string
+	latin?: string
 }
 
+export type DiagnosisWithoutLatin = Omit<Diagnosis, 'latin'>
 export enum Gender {
-  Male = "male",
-  Female = "female",
-  Other = "other"
+	Male = 'male',
+	Female = 'female',
+	Other = 'other',
 }
+
+export interface BaseEntry {
+	id: string
+	description: string
+	date: string
+	specialist: string
+	diagnosisCodes?: Diagnosis['code'][]
+}
+
+export enum HealthCheckRating {
+	'Healthy' = 0,
+	'LowRisk' = 1,
+	'HighRisk' = 2,
+	'CriticalRisk' = 3,
+}
+
+export interface HospitalEntry extends BaseEntry {
+	type: 'Hospital'
+	discharge: {
+		date: string
+		criteria: string
+	}
+}
+
+export interface OccupationalHealthcareEntry extends BaseEntry {
+	type: 'OccupationalHealthcare'
+	employerName: string
+	sickLeave?: {
+		startDate: string
+		endDate: string
+	}
+}
+
+export interface HealthCheckEntry extends BaseEntry {
+	type: 'HealthCheck'
+	healthCheckRating: HealthCheckRating
+}
+
+export type Entry =
+	| HospitalEntry
+	| OccupationalHealthcareEntry
+	| HealthCheckEntry
 
 export interface Patient {
-  id: string;
-  name: string;
-  occupation: string;
-  gender: Gender;
-  ssn?: string;
-  dateOfBirth?: string;
+	id: string
+	name: string
+	occupation: string
+	gender: Gender
+	ssn?: string
+	dateOfBirth?: string
+	entries: Entry[]
 }
 
-export type PatientFormValues = Omit<Patient, "id" | "entries">;
+export type PatientFormValues = Omit<Patient, 'id' | 'entries'>
