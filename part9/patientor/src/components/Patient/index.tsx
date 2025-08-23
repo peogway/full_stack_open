b3f557dc15dda8c3e5af303b7222ac7def10f6e2
peogway <duchungbeo2004@gmail.com>
2025-08-23 03:47:05 +0300

    add icons, improve styles, show entries, allow to add new entry

--- part9/patientor/src/components/Patient/index.tsx
+++ part9/patientor/src/components/Patient/index.tsx
@@ -1,17 +1,27 @@
 import { Patient } from '../../types'
 import { useEffect, useState } from 'react'
-import axios from 'axios'
+
+import EntryDetail from '../EntryDetails'
+import EntryForm from '../EntryForm'
+
 import patientService from '../../services/patients'
-import { apiBaseUrl } from '../../constants'
+// import diagnosisService from '../../services/diagnosis'
+
 import { useParams } from 'react-router-dom'
+import FemaleIcon from '@mui/icons-material/Female'
+import MaleIcon from '@mui/icons-material/Male'
+import { Button } from '@mui/material'
 
 const SelectedPatient = () => {
 	const { id } = useParams<{ id: string }>()
 	const [patient, setPatient] = useState<Patient | null>(null)
+	const [showEntryForm, setShowEntryForm] = useState(false)
 
 	useEffect(() => {
-		void axios.get<void>(`${apiBaseUrl}/ping`)
-
+		// const fetchDiagnosis = async () => {
+		// 	const diagnosisData = await diagnosisService.getAll()
+		// 	setDiagnosis(diagnosisData)
+		// }
 		const fetchPatient = async () => {
 			if (id) {
 				const patient = await patientService.getOne(id)
@@ -19,16 +29,55 @@ const SelectedPatient = () => {
 			}
 		}
 		void fetchPatient()
-	}, [id])
+		// void fetchDiagnosis()
+	}, [])
+
 	return (
 		<div>
 			{patient && (
 				<>
-					<p>Name: {patient.name}</p>
-					<p>Gender: {patient.gender}</p>
+					<h2>
+						{patient.name} |{' '}
+						{patient.gender === 'female' ? (
+							<FemaleIcon
+								fontSize='large'
+								style={{ transform: 'translateY(9px)' }}
+							/>
+						) : patient.gender === 'male' ? (
+							<MaleIcon
+								fontSize='large'
+								style={{ transform: 'translateY(9px)' }}
+							/>
+						) : (
+							<>Other</>
+						)}
+					</h2>
 					{patient.ssn && <p>SSN: {patient.ssn}</p>}
 					<p>Date of Birth: {patient.dateOfBirth}</p>
 					<p>Occupation: {patient.occupation}</p>
+					<br />
+					{showEntryForm && (
+						<EntryForm onclose={() => setShowEntryForm(false)} />
+					)}
+					<h3>Entries</h3>
+					{patient.entries.length > 0 ? (
+						<>
+							{patient.entries.map((entry) => (
+								<EntryDetail key={entry.id} entry={entry} />
+							))}
+						</>
+					) : (
+						<p>No entries found.</p>
+					)}
+					<Button
+						onClick={() => {
+							setShowEntryForm(true)
+						}}
+						variant='contained'
+						color='primary'
+					>
+						ADD NEW ENTRY
+					</Button>
 				</>
 			)}
 		</div>
