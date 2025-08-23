eb5ea8b14c24fa3acbcf65338645d60e96b8c90c
peogway <duchungbeo2004@gmail.com>
2025-08-23 03:45:41 +0300

    entry detail component

---
+++ part9/patientor/src/components/EntryDetails/index.tsx
@@ -0,0 +1,121 @@
+import {
+	HospitalEntry,
+	HealthCheckEntry,
+	OccupationalHealthcareEntry,
+	Entry,
+	HealthCheckRating,
+} from '../../types'
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices'
+import WorkIcon from '@mui/icons-material/Work'
+import FavoriteIcon from '@mui/icons-material/Favorite'
+import MedicationLiquidIcon from '@mui/icons-material/MedicationLiquid'
+import SubdirectoryArrowRightIcon from '@mui/icons-material/SubdirectoryArrowRight'
+
+// interface EntryProps {
+// 	entry: Entry
+// 	// diagnosis: Diagnosis[] | null
+// }
+// const findDiagnoseName = (diagnosis: Diagnosis[] | null, code: string) => {
+// 	return diagnosis?.find((diag) => diag.code === code)?.name || null
+// }
+const HospitalEntryDetails = ({ entry }: { entry: HospitalEntry }) => {
+	return (
+		<div
+			style={{
+				border: 'solid 1px',
+				borderRadius: '5px',
+				padding: '1px',
+				marginBottom: '10px',
+			}}
+		>
+			<p>
+				{entry.date}{' '}
+				<MedicationLiquidIcon style={{ transform: 'translateY(5px)' }} />
+			</p>
+			<p>{entry.description}</p>
+			{entry.discharge.criteria && (
+				<p>
+					<SubdirectoryArrowRightIcon
+						style={{ transform: 'translateY(5px)' }}
+					/>
+					{entry.discharge.criteria}
+				</p>
+			)}
+			<p>diagnose by {entry.specialist}</p>
+		</div>
+	)
+}
+
+const OccupationalHealthcareEntryDetails = ({
+	entry,
+}: {
+	entry: OccupationalHealthcareEntry
+}) => {
+	return (
+		<div
+			style={{
+				border: 'solid 1px',
+				borderRadius: '5px',
+				padding: '1px',
+				marginBottom: '10px',
+			}}
+		>
+			<p>
+				{entry.date} <WorkIcon style={{ transform: 'translateY(5px)' }} />{' '}
+				{entry.employerName}
+			</p>
+			<p>{entry.description}</p>
+			<p>diagnose by {entry.specialist}</p>
+		</div>
+	)
+}
+
+const HealthCheckEntryDetails = ({ entry }: { entry: HealthCheckEntry }) => {
+	return (
+		<div
+			style={{
+				border: 'solid 1px',
+				borderRadius: '5px',
+				padding: '1px',
+				marginBottom: '10px',
+			}}
+		>
+			<p>
+				{entry.date}{' '}
+				<MedicalServicesIcon style={{ transform: 'translateY(5px)' }} />
+			</p>
+			<p>{entry.description}</p>
+			<FavoriteIcon
+				style={{
+					color:
+						entry.healthCheckRating === 0
+							? 'green'
+							: entry.healthCheckRating === 1
+							? 'yellow'
+							: entry.healthCheckRating === 2
+							? 'red'
+							: 'black',
+					transform: 'translateY(5px)',
+				}}
+			/>{' '}
+			{HealthCheckRating[entry.healthCheckRating]}
+			<p>diagnose by {entry.specialist}</p>
+		</div>
+	)
+}
+
+const EntryDetail = ({ entry }: { entry: Entry }) => {
+	switch (entry.type) {
+		case 'HealthCheck':
+			return <HealthCheckEntryDetails entry={entry} />
+		case 'Hospital':
+			return <HospitalEntryDetails entry={entry} />
+		case 'OccupationalHealthcare':
+			return <OccupationalHealthcareEntryDetails entry={entry} />
+		default:
+			return null
+	}
+}
+
+export default EntryDetail
+
