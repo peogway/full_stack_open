f64d3ed488659e1f78adec9c9ea857e13be6d140
peogway <duchungbeo2004@gmail.com>
2025-08-24 03:55:28 +0300

    add props to entry form, update entry after added

--- part9/patientor/src/components/Patient/index.tsx
+++ part9/patientor/src/components/Patient/index.tsx
@@ -1,11 +1,11 @@
-import { Patient } from '../../types'
+import { Diagnosis, Entry, Patient } from '../../types'
 import { useEffect, useState } from 'react'
 
 import EntryDetail from '../EntryDetails'
 import EntryForm from '../EntryForm'
 
 import patientService from '../../services/patients'
-// import diagnosisService from '../../services/diagnosis'
+import diagnosisService from '../../services/diagnosis'
 
 import { useParams } from 'react-router-dom'
 import FemaleIcon from '@mui/icons-material/Female'
@@ -16,12 +16,14 @@ const SelectedPatient = () => {
 	const { id } = useParams<{ id: string }>()
 	const [patient, setPatient] = useState<Patient | null>(null)
 	const [showEntryForm, setShowEntryForm] = useState(false)
+	const [diagnosis, setDiagnosis] = useState<Diagnosis[]>([])
+	const [addedEntry, setAddedEntry] = useState<Entry | null>(null)
 
 	useEffect(() => {
-		// const fetchDiagnosis = async () => {
-		// 	const diagnosisData = await diagnosisService.getAll()
-		// 	setDiagnosis(diagnosisData)
-		// }
+		const fetchDiagnosis = async () => {
+			const diagnosisData = await diagnosisService.getAll()
+			setDiagnosis(diagnosisData)
+		}
 		const fetchPatient = async () => {
 			if (id) {
 				const patient = await patientService.getOne(id)
@@ -29,8 +31,20 @@ const SelectedPatient = () => {
 			}
 		}
 		void fetchPatient()
-		// void fetchDiagnosis()
+		void fetchDiagnosis()
 	}, [])
+
+	useEffect(() => {
+		if (addedEntry) {
+			setPatient((prev) => {
+				if (!prev) return null
+				return {
+					...prev,
+					entries: [...prev.entries, addedEntry],
+				}
+			})
+		}
+	}, [addedEntry])
 
 	return (
 		<div>
@@ -57,7 +71,12 @@ const SelectedPatient = () => {
 					<p>Occupation: {patient.occupation}</p>
 					<br />
 					{showEntryForm && (
-						<EntryForm onclose={() => setShowEntryForm(false)} />
+						<EntryForm
+							onClose={() => setShowEntryForm(false)}
+							diagnosis={diagnosis}
+							patientId={patient.id}
+							setAddedEntry={setAddedEntry}
+						/>
 					)}
 					<h3>Entries</h3>
 					{patient.entries.length > 0 ? (
